2e877aeed01d66edb8e6f0629d6851b78dd61d26
CSIDanielD <daniel.durbin@csiweb.com>
2021-03-11 14:28:53 -0600

    Added BaseFormFieldDirective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { BaseFormFieldDirective } from './directives/base-form-field.directive';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, BaseFormFieldDirective],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
